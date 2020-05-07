ce1db4cfdbc035cd3e271b4a88a179dad432d0ce
naman-gupta99 <kungfunaman@gmail.com>
2020-05-07 23:19:58 +0530

    Updates

---
+++ precache-manifest.ff89e1e40498c5c416eac2be546c27e5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d3d1c6b1fbfbfe448a99785ad977d7",
+    "url": "/covid_kanvas/index.html"
+  },
+  {
+    "revision": "46db5ca913deedb0e647",
+    "url": "/covid_kanvas/static/css/2.cc90cae1.chunk.css"
+  },
+  {
+    "revision": "80ba1b80a36bc7c53d1f",
+    "url": "/covid_kanvas/static/css/main.0d520c9b.chunk.css"
+  },
+  {
+    "revision": "46db5ca913deedb0e647",
+    "url": "/covid_kanvas/static/js/2.5c26c1bd.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/covid_kanvas/static/js/2.5c26c1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80ba1b80a36bc7c53d1f",
+    "url": "/covid_kanvas/static/js/main.f5242edb.chunk.js"
+  },
+  {
+    "revision": "88a8c854152188b6b294",
+    "url": "/covid_kanvas/static/js/runtime-main.50379750.js"
+  },
+  {
+    "revision": "cb8c0abc8f40e64622e90a609b9ca613",
+    "url": "/covid_kanvas/static/media/coronavirus.cb8c0abc.ico"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/covid_kanvas/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/covid_kanvas/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/covid_kanvas/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/covid_kanvas/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/covid_kanvas/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "524028385f8c4c884a5a303264ab705c",
+    "url": "/covid_kanvas/static/media/prevention.52402838.jpg"
+  },
+  {
+    "revision": "7be5f5d89eda643d49ad7a9564f442bb",
+    "url": "/covid_kanvas/static/media/symptoms.7be5f5d8.jpg"
+  },
+  {
+    "revision": "3aaea8f7a606436070cc5024066d3693",
+    "url": "/covid_kanvas/static/media/transmission.3aaea8f7.jpg"
+  },
+  {
+    "revision": "28f9da41b51f76ffbe8ad844a6dd7bdb",
+    "url": "/covid_kanvas/static/media/what is covid.28f9da41.jpg"
+  }
+]);
